a637eb2ca0f4ba341968dba7786c4fa83d5ad51f
Victor Gomes <info@vitormgomes.net>
2021-11-18 03:44:59 -0300

    Added Routes and Components prototypes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+   <Router>
+     <Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/Contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+   </Router>
   );
 }
 
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+    return (
+        <footer>
+            <Link to="/">Home</Link>
+            <Link to="/Contact">Contact</Link>
+        </footer> 
+    )
+}
